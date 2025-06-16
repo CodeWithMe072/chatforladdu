d9c99ca3f3ee0eac6e4464da52793e09abb42826
Sanjay <sanjaychouhan0072@gmail.com>
2025-06-17 01:58:44 +0530

    chl pd yr

--- instagram/settings.py
+++ instagram/settings.py
@@ -136,14 +136,20 @@ AUTH_USER_MODEL = 'chat.UserAB'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ Add this
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+  # ✅ Add this
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Optional for dev
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
