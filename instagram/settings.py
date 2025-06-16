6fa5d044d6b27e3caab5eaa490fd82e5e2c55b2d
Sanjay <sanjaychouhan0072@gmail.com>
2025-06-17 00:48:29 +0530

    force python 3.12 for channels compatibility

--- instagram/settings.py
+++ instagram/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tly*+2p5^c+-&pcht^=xxb5t!ne=pec-5)^=j^qdb#f$74natd')
