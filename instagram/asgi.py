48e880a4886ce4fc44e9748660ae7e0b4f9ff5de
Sanjay <sanjaychouhan0072@gmail.com>
2025-06-17 09:59:29 +0530

    chl pd yr

--- instagram/asgi.py
+++ instagram/asgi.py
@@ -5,32 +5,21 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from django.conf import settings
-from whitenoise import WhiteNoise # <--- Import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram.settings')
 django.setup()
 
-# Import your routing after django.setup()
+# Now import routing (safe after setup)
 import chat.routing
 
-# Create the base Django HTTP application
-http_application = get_asgi_application()
-
-# Wrap the HTTP application with WhiteNoise
-# This will serve static files from your STATIC_ROOT
-# It uses the STATIC_ROOT path you defined in settings.py
-application_with_static_files = WhiteNoise(http_application, root=settings.STATIC_ROOT)
-
-# Now, build the final protocol router
+# This is the standard, correct setup.
+# Django's get_asgi_application() will handle HTTP.
+# The WhiteNoise middleware (which we'll add back) will intercept static files.
 application = ProtocolTypeRouter({
-    # Use the WhiteNoise-wrapped version for HTTP requests
-    "http": application_with_static_files,
-
-    # Keep your WebSocket handling as it was
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
     ),
-})  
+})
--- instagram/settings.py
+++ instagram/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
    
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ADD THIS LINE BACK IN. This is the correct method.
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,11 +139,7 @@ AUTH_USER_MODEL = 'chat.UserAB'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     "staticfiles": {
