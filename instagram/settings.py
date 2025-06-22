fa433b18394c074d6088bcdad18cf870d54520b7
Sanjay <sanjaychouhan0072@gmail.com>
2025-06-22 11:26:20 +0530

    relosve error

--- instagram/settings.py
+++ instagram/settings.py
@@ -101,7 +101,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL")],
+            "hosts": [os.getenv("REDIS_URL", "redis://127.0.0.1:6379")],
         },
     },
 }
